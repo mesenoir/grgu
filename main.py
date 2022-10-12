b40a3bd444103a78f3906b8519fe1ab35212ac1b
mesenoir <mesenoir@gmail.com>
2022-10-12 22:17:48 +0500

    multilanguage, new buttons

--- main.py
+++ main.py
@@ -1,40 +1,92 @@
 import telebot
 from telebot import types
+import messages
 
-bot = telebot.TeleBot('')
+bot = telebot.TeleBot(messages.token)
+
+user_language = " "
 
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    mess = f"Hello, {message.from_user.first_name} {message.from_user.last_name}"
-    grodno = types.KeyboardButton('Grodno')
-    markup.add(grodno)
-    bot.send_message(message.chat.id, mess, reply_markup=markup)
+    mess = f"Здравствуйте, {message.from_user.first_name} {message.from_user.last_name}"
+    #mess = f"{messages.hello_user[f'hello_{user_language}']} {message.from_user.first_name} {message.from_user.last_name}"
+    user_language_en = types.KeyboardButton('en')
+    user_language_ru = types.KeyboardButton('ru')
+    markup.add(user_language_en, user_language_ru)
+    bot.send_message(message.chat.id, f"{mess} \nChoose your language:\nВыберите свой язык: ", reply_markup = markup)
 
 @bot.message_handler(content_types = ['text'])
 def bot_message(message):
+    global user_language
     if message.chat.type == 'private':
-        if message.text == 'Grodno':
+        if message.text == 'en':
+            user_language = 'en'
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            uni = types.KeyboardButton('Universities')
-            hsights = types.KeyboardButton('Historical sights')
-            enter = types.KeyboardButton('Entertainment')
-            catering = types.KeyboardButton('Catering establishments')
-            menu = types.KeyboardButton('Main Menu')
-            markup.add(uni,hsights,enter,catering,menu)
-            bot.send_message(message.chat.id, "Choose what you want to find ", reply_markup=markup)
-        elif message.text == 'Universities':
+            grodno = types.KeyboardButton(messages.city[f'city_{user_language}'])
+            markup.add(grodno)
+            bot.send_message(message.chat.id, "City:", reply_markup=markup)
+        elif message.text == 'ru':
+            user_language = 'ru'
             markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-            yanka = types.KeyboardButton('Yanka Kupala Grodno State University')
-            agrarian = types.KeyboardButton('Grodno Agrarian University')
-            medical = types.KeyboardButton('Grodno State Medical University')
-            menu = types.KeyboardButton('Main Menu')
-            markup.add(yanka, agrarian, medical, menu)
+            grodno = types.KeyboardButton(messages.city[f'city_{user_language}'])
+            markup.add(grodno)
+            bot.send_message(message.chat.id, "Город:", reply_markup=markup)
+        if message.text == messages.city[f'city_{user_language}']:
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            uni = types.KeyboardButton(messages.uni[f'uni_{user_language}'])
+            hsights = types.KeyboardButton(messages.hsights[f'hsights_{user_language}'])
+            enter = types.KeyboardButton(messages.ent[f'ent_{user_language}'])
+            catering = types.KeyboardButton(messages.cat[f'cat_{user_language}'])
+            menu = types.KeyboardButton(messages.menu[f'menu_{user_language}'])
+            markup.add(uni,hsights,enter,catering)
+            markup.add(menu)
             bot.send_message(message.chat.id, "Choose what you want to find ", reply_markup=markup)
-        elif message.text == "Main Menu":
+        elif message.text == messages.uni[f'uni_{user_language}']:
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            yanka = types.KeyboardButton(messages.yanka[f'yanka_{user_language}'])
+            agrarian = types.KeyboardButton(messages.agrar[f'agrar_{user_language}'])
+            medical = types.KeyboardButton(messages.med[f'med_{user_language}'])
+            menu = types.KeyboardButton(messages.menu[f'menu_{user_language}'])
+            markup.add(yanka, agrarian, medical)
+            markup.add(menu)
+            bot.send_message(message.chat.id, "Choose what you want to find ", reply_markup=markup)
+        elif message.text == messages.menu[f'menu_{user_language}']:
             start(message)
-
-
+        elif message.text == messages.yanka[f'yanka_{user_language}']:
+            markup = types.InlineKeyboardMarkup()
+            markup.add(types.InlineKeyboardButton(messages.pre[f'pre_{user_language}'], url="http://fdp.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.math[f'math_{user_language}'], url="http://mf.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.eng[f'eng_{user_language}'], url="https://fbt.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.tur[f'tur_{user_language}'], url="https://fh.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.psy[f'psy_{user_language}'], url="https://psf.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.law[f'law_{user_language}'], url="https://lf.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.phy[f'phy_{user_language}'], url="https://ftf.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.mach[f'mach_{user_language}'], url="http://fit.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.phis[f'phis_{user_language}'], url="http://ffc.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.eco[f'eco_{user_language}'], url="http://fem.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.phil[f'phil_{user_language}'], url="http://phf.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.mil[f'mil_{user_language}'], url="http://mil.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.bio[f'bio_{user_language}'], url="http://fbe.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.art[f'art_{user_language}'], url="http://art.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.bib[f'bib_{user_language}'], url="http://lib.grsu.by/"))
+            markup.add(types.InlineKeyboardButton(messages.ped[f'ped_{user_language}'], url="http://fp.grsu.by/"))
+            bot.send_message(message.chat.id, "Choose what you want to find", reply_markup=markup)
+        elif message.text == messages.agrar[f'agrar_{user_language}']:
+            bot.send_message(message.chat.id, messages.info[f'info_{user_language}'], parse_mode='html')
+        elif message.text == messages.med[f'med_{user_language}']:
+            bot.send_message(message.chat.id, messages.info[f'info_{user_language}'], parse_mode='html')
+        elif message.text == messages.hsights[f'hsights_{user_language}']:
+            bot.send_message(message.chat.id, messages.grodno[f'grodno_{user_language}'], parse_mode='html')
+            markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+            xav = types.KeyboardButton(messages.xav[f'xav_{user_language}'])
+            menu = types.KeyboardButton(messages.menu[f'menu_{user_language}'])
+            markup.add(xav)
+            markup.add(menu)
+            bot.send_message(message.chat.id, "Choose what you want to find ", reply_markup=markup)
+        elif message.text == messages.xav[f'xav_{user_language}']:
+            bot.send_message(message.chat.id, messages.xavtxt[f'xavtxt_{user_language}'], parse_mode='html')
+            bot.send_photo(message.chat.id, 'https://planetabelarus.by/upload/resize_cache/iblock/3d1/1330_887_18e21fe612b4afb807a26ecc22279a1d9/3d1f89091281efe1e6a55425c2a64735.jpg')
 
 @bot.message_handler(commands=['info'])
 def info(message):
---
+++ messages.py
@@ -0,0 +1,94 @@
+token = "5797094599:AAHTLShhcZ-Xn1OH0JBLdXYQcyah92G4W1g"
+
+hello_user = {'hello_ru': 'Здравствуйте,',
+              'hello_en': 'Hello, '}
+city = {'city_ru': 'Гродно',
+        'city_en': 'Grodno'}
+uni = {'uni_ru': 'Университеты',
+       'uni_en': 'Universities'}
+hsights = {'hsights_ru': 'Исторические Достопримечательности',
+           'hsights_en': 'Historical sights'}
+
+ent = {'ent_ru': 'Развлечения',
+           'ent_en': 'Entertainment'}
+
+cat = {'cat_ru': 'Заведения общественного питания',
+           'cat_en': 'Canteens'}
+
+menu = {'menu_ru': 'Главное меню',
+           'menu_en': 'Main Menu'}
+
+yanka = {'yanka_ru': 'Гродненский Государственный Университет имени Янки Купалы',
+           'yanka_en': 'Yanka Kupala Grodno State University'}
+
+agrar = {'agrar_ru': 'Гродненский Государственный Аграрный Университет',
+           'agrar_en': 'Grodno Agrarian University'}
+
+med = {'med_ru': 'Гродненский Государственный Медицинский Университет',
+           'med_en': 'Grodno State Medical University'}
+
+pre = {'pre_ru': 'Факультет довузовской подготовки',
+           'pre_en': 'Faculty of Pre-university Training'}
+
+math = {'math_ru': 'Факультет математики и информатики',
+           'math_en': 'Faculty of Mathematics and Computer Science'}
+
+eng = {'eng_ru': 'Инженерно-строительный факультет',
+           'eng_en': 'Faculty of Civil Engineering'}
+
+tur = {'tur_ru': 'Факультет истории, коммуникации и туризма',
+           'tur_en': 'Faculty of History, Communication and Tourism'}
+
+psy = {'psy_ru': 'Факультет психологии',
+           'psy_en': 'Faculty of Psychology'}
+
+law = {'law_ru': 'Юридический факультет',
+           'law_en': 'Faculty of Law'}
+
+phy = {'phy_ru': 'Физико-технический факультет',
+           'phy_en': 'Faculty of Physics and Technology'}
+
+mach = {'mach_ru': 'Факультет инновационных технологий машиностроения',
+           'mach_en': 'Faculty of Innovative Technologies of Mechanical Engineering'}
+
+phis = {'phis_ru': 'Факультет физической культуры',
+           'phis_en': 'Faculty of Physical Culture'}
+
+eco = {'eco_ru': 'Факультет экономики и управления',
+           'eco_en': 'Faculty of Economics and Management'}
+
+phil = {'phil_ru': 'Филологический факультет',
+           'phil_en': 'Faculty of Philology'}
+
+mil = {'mil_ru': 'Военный факультет',
+           'mil_en': 'Military Faculty'}
+bio = {'bio_ru': 'Факультет биологии и экологии',
+           'bio_en': 'Faculty of Biology and Ecology'}
+art = {'art_ru': 'Факультет искусств и дизайнa',
+           'art_en': 'Faculty of Arts and Design'}
+bib = {'bib_ru': 'Научная библиотека',
+           'bib_en': 'Scientific Library'}
+ped = {'ped_ru': 'Педагогический факультет',
+           'ped_en': 'Pedagogical Faculty'}
+info = {'info_ru': 'На данный момент тут нету информации',
+           'info_en': 'No data has been added yet'}
+
+grodno = {'grodno_ru': ' Гродно – один из самых важных культурно-исторических центров Белоруссии. Город основан в начале 12 века, и за свою историю он успел побывать в составе нескольких государств. Каждый раз, переходя во владение новой страны, город перенимал и сохранял частицу ее культуры, что не могло не отразиться на облике улиц, переулков, площадей. Удивительные достопримечательности Гродно тесно переплели в себе литовские, российские, немецкие и белорусские акценты. Неплохо сохранившаяся уникальная историческая застройка может многое рассказать о важных вехах и главных событиях в жизни города.',
+           'grodno_en': 'Grodno is one of the most important cultural and historical centers of Belarus. The city was founded at the beginning of the 12th century, and during its history it managed to visit several states. Each time, passing into the possession of a new country, the city accepted and preserved a particle of its culture, which could not but affect the appearance of streets, alleys, squares. The amazing sights of Grodno are closely intertwined with Lithuanian, Russian, German and Belarusian accents. A well-preserved unique historical building can tell a lot about important milestones and major events in the life of the city.'}
+
+xav ={'xav_ru': 'Кафедральный костёл Св. Франциска Ксаверия',
+      'xav_en': 'The Cathedral of St. Francis Xavier'}
+
+xavtxt ={'xavtxt_ru': 'На главной площади Гродно расположен Кафедральный собор Св. Франциска Ксаверия – сооружение, которое многие называют визитной карточкой города. Собор был возведен в 17 веке. Впечатление производит его продуманная, оригинальная архитектура: высокие башни, большие окна, острые шпили, стремящиеся ввысь. На одной из башен установлены старинные часы. Их возраст – более 5 столетий, но они до сих пор исправно функционируют. Главная особенность храма – его внутреннее убранство; для оформления не пожалели ни денег, ни золота, ни сил. Главный алтарь является одним из самых высоких алтарей во всей Европе (в высоту он может сравниться с семиэтажным домом). Алтарь оформлен многочисленными фигурами святых. Стены украшены невероятной красоты фресками. Все статуи и росписи отличаются высокой художественной ценностью.',
+      'xavtxt_en': 'The Cathedral of St. Francis Xavier is located on the main square of Grodno – a building that many call the hallmark of the city. The cathedral was built in the 17th century. The impression is made by its thoughtful, original architecture: tall towers, large windows, sharp spires, aspiring upwards. An antique clock is installed on one of the towers. Their age is more than 5 centuries, but they are still functioning properly. The main feature of the temple is its interior decoration; no money, no gold, no effort were spared for decoration. The main altar is one of the highest altars in all of Europe (in height it can be compared with a seven-story house). The altar is decorated with numerous figures of saints. The walls are decorated with incredibly beautiful frescoes. All statues and murals are of high artistic value. Address: Cathedral Church of St. Francis Xavier, Karl Marx Street, Grodno.'}
+
+neq ={'_ru': '',
+      '_en': ''}
+
+
+
+
+start_msg = 'Здравствуйте, уважаемый клиент!\nВас приветствует бот компании!\nПожалуйста, выберите из меню то что вас интересует.'
+order_menu = 'Заказать такси'
+info_menu = 'Информация'
+exit_menu = 'Покинуть бота'
